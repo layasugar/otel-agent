ab392af587b39cb439dd296800aa696c26fde0b3
laya <laya.sugar@gmail.com>
2022-02-14 11:34:56 +0800

    init

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"context"
-	"github.com/layasugar/otel-agent/jaegerexporter"
-	"github.com/layasugar/otel-agent/tailsamplingprocessor"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/jaegerreceiver"
 	"go.opentelemetry.io/collector/component"
-	"go.opentelemetry.io/collector/config/configmapprovider"
 	"go.opentelemetry.io/collector/service"
 	"go.opentelemetry.io/collector/service/defaultcomponents"
 	"log"
@@ -18,15 +16,15 @@ func main() {
 	}
 
 	info := component.BuildInfo{
-		Command:     "otel-collector",
-		Description: "Custom OpenTelemetry Collector distribution",
+		Command:     "otel-agent",
+		Description: "Custom OpenTelemetry agent distribution",
 		Version:     "1.0.0",
 	}
 
 	set := service.CollectorSettings{
-		BuildInfo:         info,
-		Factories:         factories,
-		ConfigMapProvider: configmapprovider.NewFile("otel-collector.yaml"),
+		BuildInfo:      info,
+		Factories:      factories,
+		ConfigProvider: service.MustNewDefaultConfigProvider([]string{"otel-agent.yaml"}, []string{}),
 	}
 
 	app, err := service.New(set)
@@ -46,27 +44,41 @@ func components() (component.Factories, error) {
 		return component.Factories{}, err
 	}
 
-	processors := []component.ProcessorFactory{
-		tailsamplingprocessor.NewFactory(),
+	// 接收器
+	receivers := []component.ReceiverFactory{
+		jaegerreceiver.NewFactory(),
 	}
-	for _, pr := range factories.Processors {
-		processors = append(processors, pr)
+	for _, pr := range factories.Receivers {
+		receivers = append(receivers, pr)
 	}
-	factories.Processors, err = component.MakeProcessorFactoryMap(processors...)
+	factories.Receivers, err = component.MakeReceiverFactoryMap(receivers...)
 	if err != nil {
 		return component.Factories{}, err
 	}
 
-	exporters := []component.ExporterFactory{
-		jaegerexporter.NewFactory(),
-	}
-	for _, pr := range factories.Exporters {
-		exporters = append(exporters, pr)
-	}
-	factories.Exporters, err = component.MakeExporterFactoryMap(exporters...)
-	if err != nil {
-		return component.Factories{}, err
-	}
+	// 处理器
+	//processors := []component.ProcessorFactory{
+	//	tailsamplingprocessor.NewFactory(),
+	//}
+	//for _, pr := range factories.Processors {
+	//	processors = append(processors, pr)
+	//}
+	//factories.Processors, err = component.MakeProcessorFactoryMap(processors...)
+	//if err != nil {
+	//	return component.Factories{}, err
+	//}
+
+	//// 导出器
+	//exporters := []component.ExporterFactory{
+	//	prometheusexporter.NewFactory(),
+	//}
+	//for _, pr := range factories.Exporters {
+	//	exporters = append(exporters, pr)
+	//}
+	//factories.Exporters, err = component.MakeExporterFactoryMap(exporters...)
+	//if err != nil {
+	//	return component.Factories{}, err
+	//}
 
 	return factories, err
 }
